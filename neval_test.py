960dd1e2e22d7c8cd0dde7039419032701409731
Andrea Pasquale <andreapasquale97@gmail.com>
2020-11-29 16:14:43 +0100

    added pineappl integrand

--- neval_test.py
+++ neval_test.py
@@ -16,18 +16,24 @@ from scipy.optimize import rosen
 
 SHOW_PLOT = False
 
-dim = 8
+dim = 3
 # data set
 n_eval_min = 1e3
 n_eval_max = 1e6
 n_evals = np.logspace(3,6,6,dtype=np.int32)
 #print(n_evals)
 n_iter = 10
+domain_per_dimension = [[0.,1.]]
+plot_title = f"Comparison for physics integrand dim = {dim}"
+file_name = "plots/dy_aa_samples.png"
 
 
 n_eval_warmup = n_eval_min
 n_iter_warmup = 5
 
+# possible integrands
+
+# 1) gaussian
 def f(x,dim=None):
     if dim is None:
         dim = x.shape[-1]
@@ -38,11 +44,73 @@ def f(x,dim=None):
         dx2 += (x[d] - 0.5) ** 2
     return math.exp(-dx2 * 100.) * coef
 
+# 2) Drell-Yan
+def int_photo(s, t, u):
+    alpha0 = 1.0 / 137.03599911
+    return alpha0 * alpha0 / 2.0 / s * (t / u + u / t)
+
+
+def hadronic_pspgen(xarr, mmin, mmax):
+    smin = mmin * mmin
+    smax = mmax * mmax
+
+    r1 = xarr[0]
+    r2 = xarr[1]
+    r3 = xarr[2]
+
+    tau0 = smin / smax
+    tau = pow(tau0, r1)
+    y = pow(tau, 1.0 - r2)
+    x1 = y
+    x2 = tau / y
+    s = tau * smax
+
+    jacobian = tau * np.log(tau0) * np.log(tau0) * r1
+
+    # theta integration (in the CMS)
+    cos_theta = 2.0 * r3 - 1.0
+    jacobian *= 2.0
+
+    t = -0.5 * s * (1.0 - cos_theta)
+    u = -0.5 * s * (1.0 + cos_theta)
+
+    # phi integration
+    jacobian *= 2.0 * np.math.acos(-1.0)
+
+    return s, t, u, x1, x2, jacobian
+
+def integrand_pineappl(xarr, dim=None, **kwargs):
+    if dim is None:
+        dim = xarr.shape[-1]
+    # in GeV^2 pbarn
+    hbarc2 = 389379372.1
+    s, t, u, x1, x2, jacobian = hadronic_pspgen(xarr, 10.0, 7000.0)
+
+    ptl = np.sqrt((t * u / s))
+    mll = np.sqrt(s)
+    yll = 0.5 * np.log(x1 / x2)
+    ylp = np.abs(yll + np.math.acosh(0.5 * mll / ptl))
+    ylm = np.abs(yll - np.math.acosh(0.5 * mll / ptl))
+
+    jacobian *= hbarc2
+
+    # cuts for LO for the invariant-mass slice containing the
+    # Z-peak from CMSDY2D11
+    if ptl < 14.0 or np.abs(yll) > 2.4 or ylp > 2.4 \
+        or ylm > 2.4 or mll < 60.0 or mll > 120.0:
+        weight = 0.0
+    else:
+        weight = jacobian * int_photo(s, u, t)
+    #print(weight)
+    #q2 = 90.0 * 90.0    
+    #grid.fill(x1, x2, q2, 0, np.abs(yll), 0, weight)
+    return weight
+
 
 def integration(integrand,isStratified,isImportance,n_eval):
 
     #assign integration volume to integrator
-    region = dim * [[-1.,1.]]
+    region = dim * domain_per_dimension
 
 
     if isStratified and isImportance:
@@ -102,7 +170,7 @@ def make_plot(importance,stratified,vegas):
 
     plt.xlabel('samples')
     ax[0].set_ylabel(' Percent uncertainty')
-    fig.suptitle(f'Comparison for Rosen function [-1,1] dim = {dim}')
+    fig.suptitle(plot_title)
     ax[0].legend()
 
     ax[1].plot(n_evals, perc_err_vegas,label="vegas+")
@@ -114,9 +182,9 @@ def make_plot(importance,stratified,vegas):
     if SHOW_PLOT:
         plt.show()
     else:
-        plt.savefig(f'rosen_samples_symm_dim{dim}.png',bbox_inches='tight')
+        plt.savefig(file_name,bbox_inches='tight')
 
 if __name__ == '__main__':
 
-    importance, stratified, vegas = generate_simulation(rosen)
+    importance, stratified, vegas = generate_simulation(integrand_pineappl)
     make_plot(importance,stratified,vegas)
