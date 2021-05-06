68b3eceb20376dd8e94d5ef11f23822b7ac2cf04
Andrea Pasquale <andreapasquale97@gmail.com>
2021-05-06 10:53:36 +0200

    first CPU CPU plots

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -97,34 +97,34 @@ def prepare_data3(data=None,data1=None,rtol=1e-2,index=None):
     vegas_iter = [i["iter"] for i in data if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == False]
     importance_iter = [i["iter"] for i in data if i["integrator"] == "VegasFlow" and i["perc_uncertainty"] == rtol]
     vegasplus_iter = [i["iter"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 1] 
-    #vegasplus1_iter = [i["iter"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2] 
+    vegasplus1_iter = [i["iter"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2] 
 
     vegas_rtol = [i["rtol_reached"] for i in data if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == False]
     importance_rtol = [i["rtol_reached"] for i in data if i["integrator"] == "VegasFlow" and i["perc_uncertainty"] == rtol]
     vegasplus_rtol = [i["rtol_reached"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 1]
-    #vegasplus1_rtol = [i["rtol_reached"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2]
+    vegasplus1_rtol = [i["rtol_reached"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2]
 
     vegas_time = [i["time"] for i in data if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == False]
     importance_time = [i["time"] for i in data if i["integrator"] == "VegasFlow" and i["perc_uncertainty"] == rtol] 
     vegasplus_time = [i["time"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 1]
-    #vegasplus1_time = [i["time"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2]
+    vegasplus1_time = [i["time"] for i in data1 if i["integrator"] == "VegasFlowPlus" and i["perc_uncertainty"] == rtol and i["adaptive"] == True and i["warmup"] == 2]
     
     df_iter = pd.DataFrame({'vegasflow' : importance_iter,
                             'vegasflowplus' : vegas_iter,
-                            'vegasflowplus adaptive' : vegasplus_iter
-                            #'vegasflowplus adaptive after warmup' : vegasplus1_iter   
+                            'vegasflowplus adaptive' : vegasplus_iter,
+                            'vegasflowplus adaptive after warmup' : vegasplus1_iter   
                             },index=index)
 
     df_time = pd.DataFrame({'vegasflow' : importance_time,
                             'vegasflowplus' : vegas_time,
-                            'vegasflowplus adaptive' : vegasplus_time
-                            #'vegasflowplus adaptive after warmup' : vegasplus1_time
+                            'vegasflowplus adaptive' : vegasplus_time,
+                            'vegasflowplus adaptive after warmup' : vegasplus1_time
                              },index=index)
 
     df_rtol= pd.DataFrame({'vegasflow' : importance_rtol,
                            'vegasflowplus' : vegas_rtol,
-                           'vegasflowplus adaptive' : vegasplus_rtol
-                           #'vegasflowplus adaptive after warmup' : vegasplus1_rtol
+                           'vegasflowplus adaptive' : vegasplus_rtol,
+                           'vegasflowplus adaptive after warmup' : vegasplus1_rtol
                             },index=index)
 
     return df_iter, df_time, df_rtol
@@ -307,9 +307,9 @@ def make_histo3(infile=None, outfile=None, save=False,title=str):
         for key in all.keys():
             data.append(all[key]) 
 
-    index = ['100000', '1000000']
+    #index = ['100000', '1000000']
     #index1 = ['1000', '10000', '100000']
-    
+    index = ['1000000']
     fig, axs  = plt.subplots(3, 3, sharey='row',figsize=(13, 10))
     fig.text(0.02, 0.5, 'samples/iteration', ha='center', va='center', rotation='vertical')
     df_iter1, df_time1, df_rtol1 = prepare_data3(data=data[0],data1=data[1],rtol=1e-2,index=index)
@@ -360,7 +360,67 @@ def make_histo3(infile=None, outfile=None, save=False,title=str):
         plt.show()
 
 
+def make_histo4(function=str, save=False, title=str):
+    
+    path_CPU= f'simulation_CPU/{function}.json'
+    path_GPU0 = f'simulation_GPU/simulation_titan/{function}.json'
+    path_GPU1 = f'simulation_GPU/simulation_rtx/{function}.json'
+    path_GPU01 = f'simulation_GPU/simulation_both_gpus/{function}.json'
 
+    index = ['CPU', 'GPU0', 'GPU1', 'GPU01']
+
+    files = [path_CPU, path_GPU0, path_GPU1, path_GPU01]
+
+    data =[]
+    for file in files:
+        with open(file, "r") as f:
+            all = json.load(f)
+            for key in all.keys():
+                data.append(all[key])
+
+    not_adaptive = data[0]+data[2]+data[4]+data[6]
+    adaptive = data[1]+data[3]+data[5]+data[7]
+
+    fig, axs  = plt.subplots(3, 3, sharey='row',figsize=(13, 10))
+    #fig.text(0.02, 0.5, 'samples/iteration', ha='center', va='center', rotation='vertical')
+    df_iter1, df_time1, df_rtol1 = prepare_data3(data=not_adaptive,data1=adaptive,rtol=1e-2,index=index)
+    df_iter2, df_time2, df_rtol2 = prepare_data3(data=not_adaptive,data1=adaptive,rtol=1e-3,index=index)
+    df_iter3, df_time3, df_rtol3 = prepare_data3(data=not_adaptive,data1=adaptive,rtol=1e-4,index=index)
+
+    fig.suptitle(title, fontsize=14)
+
+    axs[0,0] = df_time1.plot.barh(ax=axs[0,0],legend=False)
+    axs[0,1] = df_iter1.plot.barh(ax=axs[0,1],legend=False)
+    axs[0,2] = df_rtol1.plot.barh(ax=axs[0,2],legend=False)
+    axs[0,2].axvline(0.01,color='black',ls='--')
+    axs[0,2].ticklabel_format(style = 'sci', axis='x',  scilimits=(-2,-2))
+
+    axs[1,0] = df_time2.plot.barh(ax=axs[1,0],legend=False)
+    axs[1,1] = df_iter2.plot.barh(ax=axs[1,1],legend=False)
+    axs[1,2] = df_rtol2.plot.barh(ax=axs[1,2],legend=False)
+    axs[1,2].axvline(0.001,color='black',ls='--')
+    axs[1,2].ticklabel_format(style = 'sci', axis='x', scilimits=(-3,-3))
+
+    axs[2,0] = df_time3.plot.barh(ax=axs[2,0],legend=False)
+    axs[2,1] = df_iter3.plot.barh(ax=axs[2,1],legend=False)
+    axs[2,2] = df_rtol3.plot.barh(ax=axs[2,2],legend=False)
+    axs[2,2].axvline(0.0001,color='black',ls='--')
+    axs[2,2].ticklabel_format(style = 'sci', axis='x', scilimits=(-4,-4))
+
+    axs[2,0].set_xlabel('time (s)')
+    axs[2,1].set_xlabel('iterations')
+    axs[2,2].set_xlabel('rtol')
+
+
+    handles, labels = axs[0,0].get_legend_handles_labels()
+    fig.legend(handles, labels)
+    outfile = f'plots_CPU_GPU/{function}'
+    if save:
+        plt.savefig(outfile,bbox_inches='tight')
+    else:
+        plt.show()        
+
+    
     
 
 
--- plot.py
+++ plot.py
@@ -1,7 +1,27 @@
 from benchmark.utils import make_histo
 from benchmark.utils import make_histo3
+from benchmark.utils import make_histo4
 
 if __name__ == '__main__':
     #make_histo("simulation_improved/higgs/lo_final_beta_025.json",outfile="lo_training_beta_025",save=True,showPlus=True)
-    make_histo3("simulation_max_iter_200/gauss12d.json",outfile="new_plots/max_iterations_200/gauss12d", save=True,title="Perfomance comparison for Gauss 12 d")
-    #make_histo3("simulation_max_iter_200/gauss8d.json", save=False,title="Perfomance comparison for gauss 8d")
+    #make_histo3("simulation_max_iter_200/drellyan.json",outfile="new_plots/max_iterations_200/drellyan", save=True,title="Perfomance comparison for DrellYan")
+    
+    make_histo4(function='singletop',save=True,title='Comparison for singletop')
+    '''
+    make_histo3("simulation_CPU/higgs_REAL.json",  
+                outfile="CPU_plots/higgs_REAL",
+                save=True,
+                title="Perfomance comparison for higgs REAL")
+
+    
+    make_histo3("simulation_GPU/simulation_both_gpus/singletop.json",
+                outfile="GPU_plots/singletop/gpu01",
+                save=True,
+                title="Perfomance comparison for singletop")
+
+    make_histo3("simulation_GPU/simulation_rtx/singletop.json",
+                outfile="GPU_plots/singletop/gpu1",
+                save=True,
+                title="Perfomance comparison for singletop")
+    '''
+    
